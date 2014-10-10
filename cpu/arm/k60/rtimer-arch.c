618d37f3280cb080c300e80b590270c17d9fd237
Joakim Gebart <joakim.gebart@eistec.se>
2014-10-10 10:30:31 +0200

    cpu/arm/k60: Implement clock via rtimer API.
     - Change rtimer to use LPTMR (support STOP modes while rtimer is active)
     - Change clock module to use rtimer (instead of LPTMR hardware)
     - Reduce rtimer_clock_t to 32 bits (was a hassle to handle 64 bit)
     - Reduce RTIMER_SECOND to 32768 (was F_BUS == 48000000)
     - Handle the RTIMER_PERIOD overflow correctly.
     - Handle timers set beyond the reach of the LPTMR.
    
    LPTMR is only 16 bits wide and we handle rtimers beyond the 2 second limit
    of the hardware timer by keeping track of a 32 bit offset.
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- cpu/arm/k60/rtimer-arch.c
+++ cpu/arm/k60/rtimer-arch.c
@@ -40,7 +40,7 @@
 #include "rtimer-arch.h"
 #include "K60.h"
 
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
@@ -48,128 +48,107 @@
 #define PRINTF(...)
 #endif
 
-/* Convenience macro to access the channel struct directly. */
-#define RTIMER_ARCH_CHANNEL (RTIMER_ARCH_PIT_DEV->CHANNEL[RTIMER_ARCH_PIT_CHANNEL])
-/* Convenience macro to enable and disable the rtimer timer hardware. */
-#define RTIMER_ARCH_TIMER_ENABLE() (BITBAND_REG(RTIMER_ARCH_CHANNEL.TCTRL, PIT_TCTRL_TEN_SHIFT) = 1)
-#define RTIMER_ARCH_TIMER_DISABLE() (BITBAND_REG(RTIMER_ARCH_CHANNEL.TCTRL, PIT_TCTRL_TEN_SHIFT) = 0)
+/* Write some garbage to CNR to latch the current value */
+/* A bug in the MK60D10.h header causes errors since the CNR field is const
+ * declared, cast to volatile uint32_t* as a workaround. */
+#define LPTMR0_LATCH_CNR() (*((volatile uint32_t*)(&(LPTMR0->CNR))) = 0)
+
+/* The period length of the rtimer, in rtimer ticks */
+#define RTIMER_PERIOD ((LPTMR_CNR_COUNTER_MASK >> LPTMR_CNR_COUNTER_SHIFT) + 1)
+
+/* Number of ticks into the future for us to not consider the rtimer as immediately expired */
+/* at 32768 Hz (LPTMR maximum), 1 tick == 30.51 us */
+/* at 4096 Hz (msp430 default), 1 tick == 244.1 us */
+#define RTIMER_SCHEDULE_MARGIN (5)
 
 /** Offset between current counter/timer and t=0 (boot instant) */
-static rtimer_clock_t offset;
-/** set to 0 while a task is scheduled */
-static int free_running;
-/** LDVAL of the free running timer */
-#define RTIMER_ARCH_FREE_RUNNING_LDVAL (PIT_LDVAL_TSV_MASK >> PIT_LDVAL_TSV_SHIFT)
+static volatile rtimer_clock_t offset;
+static volatile rtimer_clock_t target;
 
+/*
+ * Initialize the clock module.
+ *
+ * Generates interrupt from external 32kHz crystal.
+ */
+/* TODO(Henrik) move to platform, init may differ between platforms. */
 void
 rtimer_arch_init(void) {
   offset = 0;
+  /* Setup Low Power Timer (LPT) */
 
-  /* Free running when not waiting for any rtimer tasks. */
-  free_running = 1;
+  /* Enable LPT clock gate */
+  BITBAND_REG(SIM->SCGC5, SIM_SCGC5_LPTIMER_SHIFT) = 1;
 
-  /* Enable module clock */
-  BITBAND_REG(SIM->SCGC6, SIM_SCGC6_PIT_SHIFT) = 1;
+  /* Disable timer to change settings. */
+  /* Logic is reset when the timer is disabled, TCF flag is also cleared on disable. */
+  LPTMR0->CSR = 0x00;
+  /* Prescaler bypass, LPTMR is clocked directly by ERCLK32K. */
+  LPTMR0->PSR = (LPTMR_PSR_PBYP_MASK | LPTMR_PSR_PCS(0b10));
+  LPTMR0->CMR = LPTMR_CMR_COMPARE(32767);
+  /* Enable free running mode. */
+  BITBAND_REG(LPTMR0->CSR, LPTMR_CSR_TFC_SHIFT) = 1;
+  /* Enable interrupts. */
+  BITBAND_REG(LPTMR0->CSR, LPTMR_CSR_TIE_SHIFT) = 1;
+  /* Enable timer */
+  BITBAND_REG(LPTMR0->CSR, LPTMR_CSR_TEN_SHIFT) = 1;
 
-  /* Disable timer until we have something to schedule. */
-  RTIMER_ARCH_TIMER_DISABLE();
+  /* Enable LPT interrupt */
+  NVIC_EnableIRQ(LPTimer_IRQn);
 
-  /* Enable interrupt generation */
-  BITBAND_REG(RTIMER_ARCH_CHANNEL.TCTRL, PIT_TCTRL_TIE_SHIFT) = 1;
-
-  /* Clear interrupt flag */
-  BITBAND_REG(RTIMER_ARCH_CHANNEL.TFLG, PIT_TFLG_TIF_MASK) = 1;
-
-  /* Load largest possible value to begin counting time for RTIMER_NOW. */
-  RTIMER_ARCH_CHANNEL.LDVAL = RTIMER_ARCH_FREE_RUNNING_LDVAL;
-
-  /* Enable to load the value. */
-  RTIMER_ARCH_TIMER_ENABLE();
-
-  /* Enable interrupts for PIT module in NVIC */
-  NVIC_EnableIRQ(RTIMER_ARCH_PIT_IRQn);
-
-#if DEBUG
-  int64_t err = ((int64_t)SystemBusClock) - ((int64_t)RTIMER_SECOND);
-  err *= (int64_t)1000000ull;
-  err /= (int64_t)(RTIMER_SECOND);
-  PRINTF("rtimer_arch_init: Real rtimer frequency = %lu, RTIMER_SECOND = %lu\n", (unsigned long)SystemBusClock, (unsigned long)RTIMER_SECOND);
-  PRINTF("rtimer_arch_init: Expect ca %ld ppm error on scheduled task times.\n", (signed long)err);
-#endif
   PRINTF("rtimer_arch_init: Done\n");
 }
 
 void
 rtimer_arch_schedule(rtimer_clock_t t) {
-  if (t > 0xffffffffull) {
-    PRINTF("rtimer_arch_schedule: Schedule out of range! This task will run much sooner than expected.\n");
+  rtimer_clock_t now = rtimer_arch_now();
+  if (t < (now + RTIMER_SCHEDULE_MARGIN)) {
+    /* Already happened */
+    t = now + RTIMER_SCHEDULE_MARGIN;
   }
-  /* Increase time base */
+  target = t; /* Update stored target time, read from ISR */
+  if (t > now + RTIMER_PERIOD) {
+    /* We can not reach this time in one period, wrap around */
+    t = now + RTIMER_PERIOD;
+  }
+  /* The reference manual states that the CMR register should not be written
+   * while the timer is enabled unless the TCF (interrupt flag) is set.
+  /* It seems like modifying the CMR variable without stopping (against the
+   * reference manual's recommendations) cause sporadic failures of the
+   * interrupt to trigger. It seems to happen at random. */
+  /* The downside is that the CNR register is reset when the LPTMR is disabled,
+   * we need a new offset computation. */
   offset = rtimer_arch_now();
-
+  /* Disable timer in order to modify the CMR register. */
+  BITBAND_REG(LPTMR0->CSR, LPTMR_CSR_TEN_SHIFT) = 0;
   /* Set timer value */
-  RTIMER_ARCH_CHANNEL.LDVAL = PIT_LDVAL_TSV(t);
-
-  /* No longer free running */
-  free_running = 0;
-
-  /* Disable timer and re-enable to load the new value. */
-  RTIMER_ARCH_TIMER_DISABLE();
-  RTIMER_ARCH_TIMER_ENABLE();
-
-  PRINTF("rtimer_arch_schedule: %lu\n", (unsigned long)t);
+  /* t and offset are 32 bit ints, CMR_COMPARE is only 16 bit wide,
+   * the MSBs will be cut off, so to cope with this we add an additional check
+   * for the MSBs in the ISR. */
+  LPTMR0->CMR = LPTMR_CMR_COMPARE(t - offset);
+  BITBAND_REG(LPTMR0->CSR, LPTMR_CSR_TEN_SHIFT) = 1;
 }
 
 rtimer_clock_t
 rtimer_arch_now(void) {
-  /* Timer is down-counting, we flip it here. */
-  return offset + ((rtimer_clock_t)(
-    ((RTIMER_ARCH_CHANNEL.LDVAL & PIT_LDVAL_TSV_MASK) >> PIT_LDVAL_TSV_SHIFT) -
-    ((RTIMER_ARCH_CHANNEL.CVAL & PIT_CVAL_TVL_MASK) >> PIT_CVAL_TVL_SHIFT)));
+  rtimer_clock_t cnr;
+  LPTMR0_LATCH_CNR();
+  cnr = LPTMR0->CNR; /* Read the counter value */
+  return offset + cnr;
 }
 
 /* Interrupt handler for rtimer triggers */
 void
-_isr_pit0(void) {
-  /* Save old timeout */
-  static uint32_t prev_timeout;
-  prev_timeout = ((RTIMER_ARCH_CHANNEL.LDVAL & PIT_LDVAL_TSV_MASK) >> PIT_LDVAL_TSV_SHIFT);
+_isr_lpt(void)
+{
+  rtimer_clock_t now = rtimer_arch_now();
 
-  /* Clear interrupt flag */
-  BITBAND_REG(RTIMER_ARCH_CHANNEL.TFLG, PIT_TFLG_TIF_SHIFT) = 1;
+  /* Clear timer compare flag by writing a 1 to it */
+  BITBAND_REG(LPTMR0->CSR, LPTMR_CSR_TCF_SHIFT) = 1;
 
-  /* Update offset with the time that has passed since the counter reached zero.
-   * This time offset could potentially be significant, for example when
-   * multiple interrupts occur right before the PIT interrupt causing the PIT
-   * interrupt to become pending while the other interrupts are handled, or
-   * interrupts with a higher priority cause this ISR to be */
-  /* After triggering this interrupt we are currently in, the counter will start
-   * over at LDVAL and continue counting down. This new time is added to the
-   * offset by the line below, however, we still have not added the initial
-   * length of the timer that actually triggered the interrupt. */
-  offset = rtimer_arch_now(); /* offset = [old offset] + [ISR processing time] */
-
-  /* rtimer schedulings are one-shot, go back to counting time for RTIMER_NOW */
-  /* Load largest possible value */
-  RTIMER_ARCH_CHANNEL.LDVAL = RTIMER_ARCH_FREE_RUNNING_LDVAL;
-
-  RTIMER_ARCH_TIMER_DISABLE();
-  RTIMER_ARCH_TIMER_ENABLE();
-  /* We do the above few lines as close as possible to the
-   * offset = rtimer_arch_now(); in order to avoid losing a lot of ticks. */
-
-  /* Increase time base for RTIMER_NOW by the initial timer amount. */
-  offset += (rtimer_clock_t)prev_timeout;
-  /* offset is now [offset before ISR] + [ISR processing time] + [timer interval] */
-
-  /** \todo compensate for time between offset = rtimer_arch_now() and RTIMER_ARCH_TIMER_ENABLE in rtimer ISR. */
-
-  if (free_running == 0) {
-    /* We hit a scheduled task time. */
-    free_running = 1;
-    /* Call higher level rtimer module */
+  if (target > now) {
+    /* Overflow, schedule the next period */
+    rtimer_arch_schedule(target);
+  } else {
     rtimer_run_next();
   }
-  /* Else: Timeout while counting time (happens after 89 seconds at 48MHz).
-   *  nothing to do here then but to continue counting. */
 }
