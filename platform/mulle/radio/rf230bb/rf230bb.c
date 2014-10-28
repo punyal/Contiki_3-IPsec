4243ec56d4aea279f43303be7aa7aab5da368602
Joakim Gebart <joakim.gebart@eistec.se>
2014-10-28 13:49:57 +0100

    platform/mulle: rf230: remove irq disable around LLWU calls.
    IRQ disable should not be necessary since the addition of exclusive
    operations on the low power mode inhibit counters in
    1c2a5d7bda - cpu/arm/k60: LLWU use safe counters.
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- platform/mulle/radio/rf230bb/rf230bb.c
+++ platform/mulle/radio/rf230bb/rf230bb.c
@@ -539,9 +539,7 @@ on(void)
     /* Inhibit the lowest sleep modes since we can not wake the MCU using the
      * radio IRQ pin on Mulle. (Platform board errata)
      */
-    HAL_ENTER_CRITICAL_REGION();
     LLWU_INHIBIT_LLS();
-    HAL_LEAVE_CRITICAL_REGION();
     hal_set_slptr_low();
     delay_us(2 * TIME_SLEEP_TO_TRX_OFF);
 /*  delay_us(TIME_SLEEP_TO_TRX_OFF+TIME_SLEEP_TO_TRX_OFF/2); */
@@ -579,9 +577,7 @@ off(void)
 #if RADIOSLEEPSWHENOFF
   /* Sleep Radio */
   if(!hal_get_slptr()) {
-    HAL_ENTER_CRITICAL_REGION();
     LLWU_UNINHIBIT_LLS();
-    HAL_LEAVE_CRITICAL_REGION();
     hal_set_slptr_high();
   }
 
@@ -754,9 +750,7 @@ rf230_init(void)
 
   /* Do full rf230 Reset */
   hal_set_rst_low();
-  HAL_ENTER_CRITICAL_REGION();
   LLWU_INHIBIT_LLS();
-  HAL_LEAVE_CRITICAL_REGION();
   hal_set_slptr_low();
 #if 1
   /* On powerup a TIME_RESET delay is needed here, however on some other MCU reset
@@ -905,9 +899,7 @@ rf230_transmit(unsigned short payload_len)
       }
     }
 #else
-    HAL_ENTER_CRITICAL_REGION();
     LLWU_INHIBIT_LLS();
-    HAL_LEAVE_CRITICAL_REGION();
     hal_set_slptr_low();
     DEBUGFLOW('j');
     delay_us(2 * TIME_SLEEP_TO_TRX_OFF); /* extra delay depends on board capacitance */
